520e0628824b511c73d0ce223e40807e37f9c683
Guilherme Boaviagem <guilherme.boaviagem@neurotech.com.br>
2021-05-01 23:09:20 -0300

    Add src folder.

--- minimal-blog/gatsby-config.js
+++ minimal-blog/gatsby-config.js
@@ -6,6 +6,7 @@ const shouldAnalyseBundle = process.env.ANALYSE_BUNDLE
 
 module.exports = {
   siteMetadata: {
+    siteTitle: `Guilherme Boaviagem`,
     siteTitleAlt: `Guilherme Boaviagem`,
   },
   plugins: [
